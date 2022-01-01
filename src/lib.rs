81e60108b0902a2d262df663e312b65d0890ba31
kazuma namiki <namityo@hotmail.com>
2022-01-01 23:04:20 +0900

    ドキュメントコメントを追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,32 @@
-/// # rust_reversi
-/// 
-/// Rust製のオセロクレート
-/// 
-/// # 使い方
-/// 
-/// ```
-/// extern crate rust_reversi;
-/// use rust_reversi::board::Board;
-/// use rust_reversi::piece_type::PieceType;
-/// use rust_reversi::point::Point;
-/// 
-/// let mut board = Board::new(8, 8);
-/// 
-/// let point = Point::new(4, 3);
-/// let piece_type = PieceType::Black;
-/// 
-/// if board.can_put_piece(piece_type, &point) {
-///     board = board.put_piece(piece_type, point);
-///     board.print();
-/// }
-/// 
-/// if let Some(piece_type) = board.get_winner() {
-///     println!("{} の勝利!", piece_type)
-/// } else {
-///     println!("同点")
-/// }
-/// ```
-/// 
+//! # rust_reversi
+//! 
+//! Rust製のオセロ
+//! 
+//! # 使い方
+//! 
+//! ```
+//! extern crate rust_reversi;
+//! use rust_reversi::board::Board;
+//! use rust_reversi::piece_type::PieceType;
+//! use rust_reversi::point::Point;
+//! 
+//! let mut board = Board::new(8, 8);
+//! 
+//! let point = Point::new(4, 3);
+//! let piece_type = PieceType::Black;
+//! 
+//! if board.can_put_piece(piece_type, &point) {
+//!     board = board.put_piece(piece_type, point);
+//!     board.print();
+//! }
+//! 
+//! if let Some(piece_type) = board.get_winner() {
+//!     println!("{} の勝利!", piece_type)
+//! } else {
+//!     println!("同点")
+//! }
+//! ```
+//! 
 
 pub mod board;
 pub mod piece_type;
--- src/piece_type.rs
+++ src/piece_type.rs
@@ -1,5 +1,7 @@
 use std::fmt;
 
+/// オセロの駒の種類
+/// 
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub enum PieceType {
     Black,
