0362afe8770f20b1f03ed146c9ff25af341f3dfc
kazuma namiki <namityo@hotmail.com>
2022-01-01 20:25:48 +0900

    ドキュメントのサンプルがエラーしていた問題を修正

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,17 @@
 /// # 使い方
 /// 
 /// ```
+/// extern crate rust_reversi;
+/// use rust_reversi::board::Board;
+/// use rust_reversi::piece_type::PieceType;
+/// use rust_reversi::point::Point;
+/// 
 /// let mut board = Board::new(8, 8);
 /// 
 /// let point = Point::new(4, 3);
 /// let piece_type = PieceType::Black;
 /// 
-/// board.can_put_piece(piece_type, &point) {
+/// if board.can_put_piece(piece_type, &point) {
 ///     board = board.put_piece(piece_type, point);
 ///     board.print();
 /// }
